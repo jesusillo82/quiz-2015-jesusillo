53953459ff5f794bd1b3cc58ecb10b2a3622ca10
Jesus Bernabe Imedio Perea <jesus4482@hotmail.com>
2015-07-13 22:04:15 +0200

    primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
+var quizController = require('../controllers/quiz_controller');// se añade en la version 3 primera pregunta
+
+
 /* GET home page. */
 // renderiza como respuesta la views index.ejs pasandole como ejs:embbeded javascript, el objeto title:'Quiz'
 // que sera sustituido por su valor en cada etiqueta title  <%= title %>
@@ -10,4 +14,7 @@ router.get('/', function(req, res) { // '/' ruta vacia ejemplo: localhost:3000/a
   res.render('index', { title: 'Quiz' }); 
 });
 
+// se añade en la version 3 primera pregunta
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
 module.exports = router;
